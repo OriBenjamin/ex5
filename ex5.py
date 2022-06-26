f30f28bce9e368c13565284f4bbf580833d6c04d
Ori Dekel <ori.dekel@mail.technion.ac.il>
2022-06-26 18:27:10 +0300

    functions 1 and 2

--- ex5.py
+++ ex5.py
@@ -12,8 +12,14 @@ def names_of_registered_students(input_json_path, course_name):
     :param course_name: The name of the course.
     :return: List of the names of the students.
     """
-    pass
-
+    registered_students = []
+    with open(input_json_path, 'r') as f:
+        student_data = json.load(f)
+        for id in student_data.keys():
+            student_courses = student_data[id]["registered_courses"]
+            if course_name in set(student_courses):
+                registered_students.append(student_data[id]["student_name"])
+    return registered_students  
 
 def enrollment_numbers(input_json_path, output_file_path):
     """
@@ -23,9 +29,19 @@ def enrollment_numbers(input_json_path, output_file_path):
     :param input_json_path: Path of the students database json file.
     :param output_file_path: Path of the output text file.
     """
-    pass
-
-
+    enrollment_dictionary = {}
+    with open(input_json_path, 'r') as f:
+        student_data = json.load(f)
+        for id in student_data.keys():
+            for course in student_data[id]["registered_courses"]:
+                if course in enrollment_dictionary:
+                    enrollment_dictionary[course] += 1
+                else:
+                    enrollment_dictionary[course] = 1
+    with open(output_file_path, 'w') as f:
+        for course, students_number in sorted(enrollment_dictionary.items()):
+            output_line = '"' + course + '" ' + str(students_number) + '\n'
+            f.write(output_line)
 
 def courses_for_lecturers(json_directory_path, output_json_path):
     """
